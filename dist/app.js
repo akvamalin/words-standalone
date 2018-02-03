83689d570ca7cdec4cec14db9d6b688daacf8f12
YevheniiMaliavka <Yevhenii.Maliavka@mercateo.com>
2018-02-03 08:14:57 +0100

    Initialize app

---
+++ dist/app.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var url_1 = require("url");
+var path_1 = require("path");
+var window = null;
+var createWindow = function () {
+    window = new electron_1.BrowserWindow({
+        width: 800,
+        height: 640
+    });
+    window.loadURL(url_1.format({
+        pathname: path_1.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    window.webContents.openDevTools();
+    window.on('closed', function () {
+        window = null;
+    });
+};
+electron_1.app.on('ready', createWindow);
+electron_1.app.on('window-all-closed', function () { return electron_1.app.quit(); });
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron';
+import { format } from 'url';
+import { join } from 'path';
+
+let window: BrowserWindow = null;
+
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 800,
+    height: 640
+  });
+
+  window.loadURL(
+    format({
+      pathname: join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+  );
+
+  window.webContents.openDevTools();
+  window.on('closed', () => {
+    window = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => app.quit());
